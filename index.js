ddb83fc2ec31ab7feccb8e6ce9c08582e56111a0
rpyoungdale <youngdale95@gmail.com>
2018-04-25 20:30:11 -0400

    real

---
+++ index.js
@@ -0,0 +1,21 @@
+function countdown (callback) {
+  window.setTimeout(callback);
+};
+
+function createMultiplier (multiplierValue) {
+  return function (num) {
+    return num*multiplierValue;
+  }
+};
+
+function doubler (num) {
+  return createMultiplier(2)(num);
+}
+
+function tripler (num) {
+  return createMultiplier(3)(num);
+}
+
+function multiplier (num1, num2) {
+  return num1*num2
+}
